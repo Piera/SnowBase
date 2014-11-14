d366119676008fa0d4250b1a34aa44196bba0c53
Piera <pierad@gmail.com>
2014-11-14 15:39:10 -0800

    Added depth, new snow to response, added narratives, updated map markers.

--- MVC/finder.py
+++ MVC/finder.py
@@ -23,14 +23,11 @@ def lookup():
 	l = request.values.get("lat", 0, type=float)
 	g = request.values.get("lng", 0, type=float)
 	print l, g
-# function 1. Take lat long form input from user (main.html):
-# Iterate through stations.latitude, stations.longitude and use haversine function to find closest stations
-#  closest stations that are reporting, and where snow depth > 0.
+	# Iterate through stations.latitude, stations.longitude and use haversine function to find closest stations
+	#  closest stations that are reporting, and where snow depth > 0.
 	dist_list = []
 	s = dbsession.query(model.Station).all()
 	for counter in s:
-		#s = dbsession.query(model.Station).get(counter)
-		# use .all to get all items in the db, can iterate over it.
 		try: 
 			u = counter.snow_data[-1]
 			# use if not != None... or [] -- try/except can hide other errors!
@@ -51,24 +48,27 @@ def lookup():
 			kms = int(distance(origin, destination))
 			mi = int(0.621371*kms)
 	closest_sta = sorted(dist_list, key=lambda k: k['dist'])[0:10]
+	print "Closest stations: ", closest_sta
+	deepest_snow = sorted(closest_sta, key=lambda k: k['depth'], reverse=True)[0:10]
+	print "Deepest snow: ", deepest_snow
+	most_new_snow = sorted(closest_sta, key=lambda k: k['depth_change'], reverse=True)[0:10]
+	print "Most new snow: ", most_new_snow
+
 	# Return the 10 closest stations, their distances away in miles (converted from kms)
 	#  and basic telemetry data for that station
 	for locations in closest_sta:
 		# print 'The', locations.get('name'), 'station is', locations.get('dist'), 'miles away. There are',\
 		#  locations.get('depth'), "inches of snow there."
-		session["l"] = {"snow":(locations.get('lat'), locations.get('lng'))}
-	# print session
+		session["l"] = {"input":(locations.get('lat'), locations.get('lng'))}
 	# response = make_response(render_template('report_singlepage.html', closest_sta=closest_sta))
-	response = json.dumps(closest_sta)
+	response = json.dumps({"closest": closest_sta, "deepest": deepest_snow, "most new": most_new_snow})
 	# print type(response)
 	return response
 
 	# Next:
 	# Make this better - look at 5 stations (or radius?), if no snow, look at next 5 closest stations (or wider radius).
 	#  or take radius input.
-	# Write function for returning deepest snow (range?)
-	# Write function for returning newest snow (range?)
-	# Write function for returning best snow quality (based on water_equiv)
+	# Return best snow quality (based on water_equiv)
 
 if __name__ == "__main__":
     app.run(debug = True)

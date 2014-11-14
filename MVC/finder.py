5babb41fce43908c1686c45f23ec379e804d5b08
Piera <pierad@gmail.com>
2014-11-13 21:52:55 -0800

    Connected map to server: gets geocode, plots result markers

--- MVC/finder.py
+++ MVC/finder.py
@@ -6,48 +6,63 @@ import operator
 from haversine import distance
 from jinja2 import Template
 from sqlalchemy import desc
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, session
 from model import session as dbsession
 
 app = Flask(__name__)
+
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 @app.route("/")
 def index():
     return render_template("main.html")
 
-@app.route("/report", methods = ['POST'])
+@app.route("/report", methods = ['GET','POST'])
 def lookup():
-	l = request.form['lat']
-	g = request.form['long']
+	# From Google maps API:
+	l = request.values.get("lat", 0, type=float)
+	g = request.values.get("lng", 0, type=float)
+	print l, g
 # function 1. Take lat long form input from user (main.html):
 # Iterate through stations.latitude, stations.longitude and use haversine function to find closest stations
 #  closest stations that are reporting, and where snow depth > 0.
 	dist_list = []
-	for counter in range(1, 868):
-		s = dbsession.query(model.Station).get(counter)
+	s = dbsession.query(model.Station).all()
+	for counter in s:
+		#s = dbsession.query(model.Station).get(counter)
+		# use .all to get all items in the db, can iterate over it.
 		try: 
-			# s.snow_data[0]
-			u = s.snow_data[-1]
-			print s.id, s.given_id, s.name, s.latitude, s.longitude, u.sta_given_id, u.depth, u.depth_change, u.date
+			u = counter.snow_data[-1]
+			# use if not != None... or [] -- try/except can hide other errors!
 			origin = float(l), float(g)
-			destination = float(s.latitude), float(s.longitude)
+			destination = float(counter.latitude), float(counter.longitude)
 			kms = int(distance(origin, destination))
 			mi = int(0.621371*kms)
-			if u.depth > 0:
-				dist_list.append({'dist':mi, 'id':s.given_id, 'name':s.name, 'depth':u.depth, 'depth_change':u.depth_change})
+			if u.depth != None and u.depth > 0:
+				# print s.id, s.given_id, s.name, s.latitude, s.longitude, u.sta_given_id,\
+				#  u.depth, u.depth_change, u.date
+				dist_list.append({'dist':mi, 'id':counter.given_id, 'lat': counter.latitude, 'lng': counter.longitude, 'name':counter.name, 'depth':u.depth,\
+				 'depth_change':u.depth_change})
+			else:
+				continue
 		except IndexError:
-			print s.id, s.given_id, s.latitude, s.longitude
 			origin = 34.134115, -118.321548
-			destination = s.latitude, s.longitude
+			destination = counter.latitude, counter.longitude
 			kms = int(distance(origin, destination))
 			mi = int(0.621371*kms)
 	closest_sta = sorted(dist_list, key=lambda k: k['dist'])[0:10]
 	# Return the 10 closest stations, their distances away in miles (converted from kms)
 	#  and basic telemetry data for that station
 	for locations in closest_sta:
-		print 'The', locations.get('id'), 'station is', locations.get('dist'), 'miles away. There are', locations.get('depth'), "inches of snow there."
-	return render_template ("report.html", closest_sta = closest_sta, lat = l, lng = g)
-	
+		# print 'The', locations.get('name'), 'station is', locations.get('dist'), 'miles away. There are',\
+		#  locations.get('depth'), "inches of snow there."
+		session["l"] = {"snow":(locations.get('lat'), locations.get('lng'))}
+	# print session
+	# response = make_response(render_template('report_singlepage.html', closest_sta=closest_sta))
+	response = json.dumps(closest_sta)
+	# print type(response)
+	return response
+
 	# Next:
 	# Make this better - look at 5 stations (or radius?), if no snow, look at next 5 closest stations (or wider radius).
 	#  or take radius input.

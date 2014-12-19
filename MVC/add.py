8e333d21cec967821478554c025d8d2375ec7c2c
Piera <pierad@gmail.com>
2014-12-18 22:30:45 -0800

    Updated add.py and scan.py for PostgreSQL seeding, data updating

--- MVC/add.py
+++ MVC/add.py
@@ -10,47 +10,33 @@ from sqlalchemy import Table, Column, Float, Integer, Boolean, String, MetaData,
 
 # Fill the database of snow data with daily telemetry data points
 def load_snow_data(session):
+	print "Session!"
 	telemetry_data = None
 # 1. Open the list of URLs for the API calls, iterate the API calls
 	with open('/Users/pieradamonte/Dropbox/Hackbright/HBProject/Cron/APIurls.csv', 'rb') as csvfile:
+		print "CSV Opened!!"
 		snow_reader = csv.reader(csvfile)
 		for row in snow_reader:
+			print "Reading rows in csv"
 			req = urllib2.Request(row[0])
 			response = urllib2.urlopen(req)
 			snow_data = json.load(response)
 # 2. Parse the resultant JSON and add the parsed JSON directly into the Snow.db snow_data table
+			# Check to see if there is data
 			if snow_data['data'] != [] and snow_data['data'][0]['Date'] != None:
+				print "There is data, now checking for snow depth"
+
+				# Check to see if there is snow depth data point
 				if snow_data['data'][0]['Snow Depth (in)'] != None:
+					# If there is snow depth data, proceed with data addition or update
 					triplet = snow_data['station_information']['triplet']
 					station_id = session.query(model.Station).filter_by(given_id=triplet).one()
-					entries = session.query(model.Snow_Data).filter_by(station_id=station_id.id)
-					last_entry = entries[-1]
-					# Compare the dates; if date is the same, update the database
-					if datetime.date(last_entry.date) == datetime.date(datetime.now()):
-						last_entry.source = 'SNOTEL'
-						last_entry.units = 'in'
-						last_entry.date = datetime.now()
-						last_entry.depth = int(snow_data['data'][0]['Snow Depth (in)'])
-						depth_change = None
-						water_equiv = None
-						water_equiv_change = None
-						if snow_data['data'][0]['Change In Snow Depth (in)'] != None:
-							last_entry.depth_change = int(snow_data['data'][0]['Change In Snow Depth (in)'])
-						if snow_data['data'][0]['Snow Water Equivalent (in)'] != None:
-							last_entry.water_equiv = float(snow_data['data'][0]['Snow Water Equivalent (in)'])
-						if snow_data['data'][0]['Change In Snow Water Equivalent (in)'] != None:
-							last_entry.water_equiv_change = float(snow_data['data'][0]['Change In Snow Water Equivalent (in)'])
-						if depth_change != None:
-							last_entry.depth_change = depth_change
-						if water_equiv != None:
-							last_entry.water_equiv = water_equiv
-						if water_equiv_change != None:
-							last_entry.water_equiv_change = water_equiv_change
-						else:
-							continue
-						session.commit()
-					# If the dates are different, then create a new entry in the database
-					else:
+					entries = session.query(model.Snow_Data).filter_by(station_id=station_id.id).all()
+					print entries
+
+					# What if there are no snow data entries for the station? Create a new one.
+					if not entries:
+						print "EXCEPT"
 						source = 'SNOTEL'
 						units = 'in'
 						date = datetime.now()
@@ -77,10 +63,74 @@ def load_snow_data(session):
 							telemetry_data.water_equiv = water_equiv
 						if water_equiv_change != None:
 							telemetry_data.water_equiv_change = water_equiv_change
-						else:
-							continue
 						session.add(telemetry_data)
+						print "Adding telemetry data, inside the EXCEPT."
+
+					# Otherwise: Compare the dates; if date is the same, update the database
+					elif entries:
+						last_entry = entries[-1]
+						datetime.date(last_entry.date) == datetime.date(datetime.now())
+						# last_entry = entries[-1]
+						print "Dates are same: update data"
+						last_entry.source = 'SNOTEL'
+						last_entry.units = 'in'
+						last_entry.date = datetime.now()
+						last_entry.depth = int(snow_data['data'][0]['Snow Depth (in)'])
+						depth_change = None
+						water_equiv = None
+						water_equiv_change = None
+						if snow_data['data'][0]['Change In Snow Depth (in)'] != None:
+							last_entry.depth_change = int(snow_data['data'][0]['Change In Snow Depth (in)'])
+						if snow_data['data'][0]['Snow Water Equivalent (in)'] != None:
+							last_entry.water_equiv = float(snow_data['data'][0]['Snow Water Equivalent (in)'])
+						if snow_data['data'][0]['Change In Snow Water Equivalent (in)'] != None:
+							last_entry.water_equiv_change = float(snow_data['data'][0]['Change In Snow Water Equivalent (in)'])
+						if depth_change != None:
+							last_entry.depth_change = depth_change
+						if water_equiv != None:
+							last_entry.water_equiv = water_equiv
+						if water_equiv_change != None:
+							last_entry.water_equiv_change = water_equiv_change
+						session.commit()
+						print "Committing data where dates are same"
+
+					# If the dates are different, then create a new entry in the database
+					else:
+						print "ELSE"
+						source = 'SNOTEL'
+						units = 'in'
+						date = datetime.now()
+						depth = int(snow_data['data'][0]['Snow Depth (in)'])
+						depth_change = None
+						water_equiv = None
+						water_equiv_change = None
+						if snow_data['data'][0]['Change In Snow Depth (in)'] != None:
+							depth_change = int(snow_data['data'][0]['Change In Snow Depth (in)'])
+						if snow_data['data'][0]['Snow Water Equivalent (in)'] != None:
+							water_equiv = float(snow_data['data'][0]['Snow Water Equivalent (in)'])
+						if snow_data['data'][0]['Change In Snow Water Equivalent (in)'] != None:
+							water_equiv_change = float(snow_data['data'][0]['Change In Snow Water Equivalent (in)'])
+						telemetry_data = model.Snow_Data(\
+							sta_given_id = triplet,\
+							station_id = station_id.id,\
+							source = source,\
+							units = units,\
+							date = date,\
+							depth = depth)
+						if depth_change != None:
+							telemetry_data.depth_change = depth_change
+						if water_equiv != None:
+							telemetry_data.water_equiv = water_equiv
+						if water_equiv_change != None:
+							telemetry_data.water_equiv_change = water_equiv_change
+						session.add(telemetry_data)
+						print "Adding telemetry data, outside of exception."
+				else:
+					continue
+			else:
+				continue
 		session.commit()
+		print "COMMIT at end of parsing and everything"
 
 def main(session):
 	load_snow_data(session)
@@ -89,3 +139,4 @@ def main(session):
 if __name__ == "__main__":
     s = model.add_data()
     main(s)
+    
--- MVC/scan.py
+++ MVC/scan.py
@@ -11,21 +11,28 @@ TWILIO_NUMBER = os.environ.get('TWILIO_NUMBER')
 
 # Scans for alerts, sends alerts, updates alerts
 def alert_scan(session):
-	u = None
+	station = None
 	all_alerts = session.query(model.Alert).all()
 	client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-	for item in all_alerts:
-		u = item.station_id
-		report = session.query(model.Station).get(u)
-		depth = report.snow_data[-1].depth
-		depth_change = report.snow_data[-1].depth_change
+	for alert in all_alerts:
+		station = alert.station_id
+		report = session.query(model.Station).get(station)
+
+		# Check for depth data for station:
+		try:
+			depth = report.snow_data[-1].depth
+			depth_change = report.snow_data[-1].depth_change
+		except IndexError:
+			depth_change = 0
+
+		# If there is data, check if there is depth change and send alert:
 		if depth_change > 0:
-			if item.status==1:
-				update = "New snow alert! Station: %s, Snow depth: %s in., Depth change: %s in. To reset this alert, respond to this text with %s." % (report.name, depth, depth_change, u)
+			if alert.status==True:
+				update = "New snow alert! Station: %s, Snow depth: %s in., Depth change: %s in. To reset this alert, respond to this text with %s." % (report.name, depth, depth_change, station)
 				message = client.messages.create(from_=TWILIO_NUMBER,
-											to=item.phone_number,
+											to=alert.phone_number,
 											body=update)
-				update_alert(item.id, item.status)
+				update_alert(alert.id, alert.status)
 		else:
 			continue
 

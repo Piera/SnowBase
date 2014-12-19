0ad7cce0c9c708367d238c1a69897d2fb1a5f9b6
Piera <pierad@gmail.com>
2014-12-19 15:31:04 -0800

    Updated clock and add.py

--- MVC/clock.py
+++ MVC/clock.py
@@ -1,18 +1,30 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-from add.py import load_snow_data
-from add.py import alert_scan
+from add import load_snow_data
+import model
+# from add import alert_scan
+# import model
 
-sched = BlockingScheduler()
+# sched = BlockingScheduler()
+# @sched.scheduled_job('interval', minutes=5)
+# sched.add_job(load_snow_data, 'interval', minutes=1)
+def timed_job():
+	load_snow_data()
 
-# @sched.scheduled_job('interval', minutes=3)
-# def timed_job(session):
-# 	load_snow_data(session)
+# sched.add_job(timed_job, 'interval', minutes=1)
+# @sched.scheduled_job('cron', day_of_week='mon-sun', hours=5)
+# def scheduled_job():
+#   load_snow_data(session)
 # 	alert_scan(session)
-    
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=4)
-def scheduled_job():
-    load_snow_data(session)
-	alert_scan(session)
+if __name__ == '__main__':
+    scheduler = BlockingScheduler()
+    scheduler.add_job(timed_job, 'interval', minutes=1)
+    # print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+    print 'OK, I am running'
 
-sched.start()
+    try:
+        scheduler.start()
+    except (KeyboardInterrupt, SystemExit):
+        pass
+
+# sched.start()
